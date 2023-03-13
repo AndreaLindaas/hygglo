19f87686cad9e844f685e70930d5e54ab95cdf7b
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-03-13 21:05:50 +0100

    added total income

--- js/index.js
+++ js/index.js
@@ -11,15 +11,18 @@ async function getTools() {
 function showTools(tools) {
   const headerLi = `<li><span><strong>Ting</strong></span><span><strong>Pris</strong></span><span><strong>Inntjening</strong></span></li>`;
   items.innerHTML = headerLi;
+  let totalIncome = 0;
   for (let i = 0; i < tools.length; i++) {
-    itemNubmer.innerHTML = `Du leier ut <strong>${tools.length}</strong> ting og har tjent kroner:`;
     let tool = `<li><span><a href="item.html?id=${tools[i].id}">${
       tools[i].name
     }</a></span><span>${tools[i].itemPrice},-</span><span>${
       tools[i].income ? tools[i].income : "0"
     },-</span></li>`;
     items.innerHTML += tool;
+
+    totalIncome += tools[i].income ? parseInt(tools[i].income) : 0;
   }
+  itemNubmer.innerHTML = `Du leier ut <strong>${tools.length}</strong> ting og har tjent ${totalIncome} kroner:`;
 }
 
 getTools();
